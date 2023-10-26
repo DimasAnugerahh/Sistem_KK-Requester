04148d767e2c45371c4c99e31aeb0766e8647f02
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-10-26 19:30:52 +0800

    feat:  add new routes

--- main.go
+++ main.go
@@ -23,6 +23,26 @@ func main() {
 	AccountService := service.NewAccountService(AccountRepository)
 	AccountController := controller.NewAccountController(AccountService)
 
+	KTPRepository := repository.NewKTPRepository(db)
+	KTPService := service.NewKTPService(KTPRepository)
+	KTPController := controller.NewKTPController(KTPService)
+
+	AktaKelahiranRepository := repository.NewAktaKelahiranRepository(db)
+	AktaKelahiranService := service.NewAktaKelahiranService(AktaKelahiranRepository)
+	AktaKelahiranController := controller.NewAktaKelahiranController(AktaKelahiranService)
+
+	SuratPindahRepository := repository.NewSuratPindahRepository(db)
+	SuratPindahService := service.NewSuratPindahService(SuratPindahRepository)
+	SuratPindahController := controller.NewSuratPindahController(SuratPindahService)
+
+	SuratNikahRepository := repository.NewSuratNikahRepository(db)
+	SuratNikahService := service.NewSuratNikahService(SuratNikahRepository)
+	SuratNikahController := controller.NewSuratNikahController(SuratNikahService)
+
+	SuratKematianRepository := repository.NewSuratKematianRepository(db)
+	SuratKematianService := service.NewSuratKematianService(SuratKematianRepository)
+	SuratKematianController := controller.NewSuratKematianController(SuratKematianService)
+
 	app.Use(middleware.LoggerWithConfig(
 		middleware.LoggerConfig{
 			Format: "method=${method}, uri=${uri}, status=${status}, time=${time_rfc3339}\n",
@@ -30,6 +50,11 @@ func main() {
 	))
 
 	routes.NewAccountRoutes(app, AccountController)
+	routes.NewKTPRoutes(app, KTPController)
+	routes.NewAktaKelahiranRoutes(app, AktaKelahiranController)
+	routes.NewSuratPindahRoutes(app, SuratPindahController)
+	routes.NewSuratKematianRoutes(app, SuratKematianController)
+	routes.NewSuratNikahRoutes(app, SuratNikahController)
 
 	app.Start(":8080")
 }
