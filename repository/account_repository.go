4f5c4244e0ebb4bec948ef9d0198c28e3ac743aa
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-10-26 19:28:43 +0800

    fix: change parameter name

--- repository/account_repository.go
+++ repository/account_repository.go
@@ -9,6 +9,6 @@ type AccountRepository interface {
 	GetAccounts(id int) (*domain.Account, error)
 	CreateAccount(NewAccount *domain.Account) (*domain.Account, error)
 	AccountLogin(Account *domain.Account) (*domain.Account, error)
-	AccountUpdate(UpdatedAccountAccount *domain.Account, id float64) (*domain.Account, error)
+	AccountUpdate(UpdatedAccount *domain.Account, id float64) (*domain.Account, error)
 	AccountDelete(DeletedAccount *domain.Account, id int) (*domain.Account, error)
 }
