1917353642d7461d4265de505e93807a790359c1
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-10-26 19:27:03 +0800

    feat:  add ktp model

--- model/domain/KTP.go
+++ model/domain/KTP.go
@@ -4,4 +4,9 @@ import "gorm.io/gorm"
 
 type KTP struct {
 	*gorm.Model
+
+	File_ktp      string          `json:"file_ktp" form:"file_ktp"`
+	Nama_lengkap  string          `json:"nama_lengkap" form:"nama_lengkap"`
+	AccountId     uint            `json:"account_id" form:"account_id"`
+	RequestDetail []RequestDetail `gorm:"foreignKey:KTPId"`
 }
