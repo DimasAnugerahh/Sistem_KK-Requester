2caad869d886cdbfe237cdbc1ea4e74622b9cd06
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-10-26 19:26:42 +0800

    feat: add account model struct

--- model/domain/Account.go
+++ model/domain/Account.go
@@ -1,12 +1,20 @@
 package domain
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Account struct {
 	*gorm.Model
 
-	Email    string `json:"email" form:"email" validate:"required,max=200,min=1"`
-	Name     string `json:"name" form:"name" validate:"required,max=50,min=1"`
-	Password string `json:"password" form:"password" validate:"required,max=50,min=1"`
-	Role     string `json:"role" form:"role" validate:"required,max=5,min=4"`
+	Email         string          `json:"email" form:"email" validate:"required,max=200,min=1"`
+	Name          string          `json:"name" form:"name" validate:"required,max=50,min=1"`
+	Password      string          `json:"password" form:"password" validate:"required,max=50,min=1"`
+	Role          string          `json:"role" form:"role" validate:"required,max=5,min=4"`
+	AktaKelahiran []AktaKelahiran `gorm:"foreignKey:AccountId"`
+	KTP           []KTP           `gorm:"foreignKey:AccountId"`
+	SuratKematian []SuratKematian `gorm:"foreignKey:AccountId"`
+	SuratNikah    []SuratNikah    `gorm:"foreignKey:AccountId"`
+	SuratPindah   []SuratPindah   `gorm:"foreignKey:AccountId"`
+	Request       []Request       `gorm:"foreignKey:AccountId"`
 }
