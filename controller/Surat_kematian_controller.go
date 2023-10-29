e1f4783da4061ada189bc47322cd7f38e3b6fa15
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-10-29 23:55:57 +0800

    feat : reformat status code

--- controller/Surat_kematian_controller.go
+++ controller/Surat_kematian_controller.go
@@ -2,8 +2,8 @@ package controller
 
 import "github.com/labstack/echo/v4"
 
-type SuratKematianController interface {
-	CreateSuratKematian() echo.HandlerFunc
-	SuratKematianUpdate() echo.HandlerFunc
-	GetSuratKematian() echo.HandlerFunc
+type AktaKematianController interface {
+	CreateAktaKematian() echo.HandlerFunc
+	AktaKematianUpdate() echo.HandlerFunc
+	GetAktaKematian() echo.HandlerFunc
 }
