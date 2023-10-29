33ad391e4d9bc9a797eb70aaea0a85f7b96f50c8
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-10-29 23:58:05 +0800

    feat : add new parameter

--- service/ktp_service.go
+++ service/ktp_service.go
@@ -7,7 +7,7 @@ import (
 )
 
 type KTPService interface {
-	CreateKTP(ctx echo.Context, NewKTP *domain.KTP) (*domain.KTP, error)
+	CreateKTP(ctx echo.Context, NewKTP *domain.KTP, request *domain.ReqDetailKtp) (*domain.KTP, *domain.ReqDetailKtp, error)
 	KTPUpdate(ctx echo.Context, UpdatedKTP *domain.KTP, ktpId float64, accountId uint) (*domain.KTP, error)
-	GetKTP(ctx echo.Context, accountId uint) ([]domain.KTP, error)
+	GetKTP(ctx echo.Context, idAccount uint) ([]domain.KTP, error)
 }
