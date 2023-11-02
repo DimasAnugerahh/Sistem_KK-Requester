9ea0987b789985b87f83bf4209a42f2e65585349
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-02 22:50:56 +0800

    refactor: remove CreateAccountResponse struct

--- model/web/account_response.go
+++ model/web/account_response.go
@@ -2,29 +2,14 @@ package web
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type AccountResponse struct {
-	*gorm.Model
-
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt time.Time
 	Email     string
 	Name      string
 	Password  string
-	Role      string
-}
-
-type CreateAccountResponse struct {
-	*gorm.Model
-
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt time.Time
-	Email     string
-	Name      string
 	Role      string
 }
