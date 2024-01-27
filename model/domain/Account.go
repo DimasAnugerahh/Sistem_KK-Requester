353d92e949c9bc5f8e25ffc2e8bb41a64f188529
DimasAnugerahh <dimasanugrah883@gmail.com>
2024-01-27 22:50:19 +0800

    refactor: update model

--- model/domain/Account.go
+++ model/domain/Account.go
@@ -11,11 +11,11 @@ type Account struct {
 	Name          string          `json:"name" form:"name" validate:"required,max=50,min=1"`
 	Password      string          `json:"password" form:"password" validate:"required,max=50,min=1"`
 	Role          string          `json:"role" form:"role" validate:"required,max=5,min=4"`
-	AktaKelahiran []AktaKelahiran `gorm:"foreignKey:AccountId" json:"omitempty"`
-	KTP           []KTP           `gorm:"foreignKey:AccountId" json:"omitempty" `
-	AktaKematian  []AktaKematian  `gorm:"foreignKey:AccountId" json:"omitempty"`
-	SuratNikah    []SuratNikah    `gorm:"foreignKey:AccountId" json:"omitempty"`
-	SuratPindah   []SuratPindah   `gorm:"foreignKey:AccountId" json:"omitempty"`
-	RequestKK     []RequestKK     `gorm:"foreignKey:AccountId" json:"omitempty"`
-	KK            []KK            `gorm:"foreignKey:AccountId" json:"omitempty"`
+	AktaKelahiran []AktaKelahiran `gorm:"foreignKey:AccountId" json:"akta_kelahiran,omitempty"`
+	KTP           []KTP           `gorm:"foreignKey:AccountId" json:"ktp,omitempty"`
+	AktaKematian  []AktaKematian  `gorm:"foreignKey:AccountId" json:"akta_kematian,omitempty"`
+	SuratNikah    []SuratNikah    `gorm:"foreignKey:AccountId" json:"surat_nikah,omitempty"`
+	SuratPindah   []SuratPindah   `gorm:"foreignKey:AccountId" json:"surat_pindah,omitempty"`
+	RequestKK     []RequestKK     `gorm:"foreignKey:AccountId" json:"request,omitempty"`
+	KK            []KK            `gorm:"foreignKey:AccountId" json:"kk,omitempty"`
 }
